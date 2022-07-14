3c75714a4587bfc0cfcdfbea542e886ea6bd6849
s1290214 <109269834+s1290214@users.noreply.github.com>
2022-07-14 16:37:02 +0900

    "HoT"最初のバージョン

---
+++ ex4.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+public class DM4 {
+    public static void main(String[] args) {
+	Random r = new Random();
+	int rand;
+	int heads = 0, tails = 0;
+
+	System.out.println("Tossing a coin ...");
+	int i = 1;
+	while(i<=3){
+	    rand = r.nextInt(2);
+	    switch(rand){
+	    case 0:
+	    heads ++;
+	    System.out.println("Round "+i+": Heads");
+	    break;
+
+	    case 1:
+		tails ++;
+		System.out.println("Round "+i+": Tails");
+		break;
+	    }
+	    i++;
+	}
+	System.out.println("Heads: "+heads+", Tails: "+tails);
+    }
+}
